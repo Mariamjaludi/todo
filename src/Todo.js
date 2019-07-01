9bcda8d09898ccea22ce5e97ef22eb272f9ecc05
mariamjaludi <mariam.jaludi@gmail.com>
2019-07-01 21:24:58 +0100

    form added

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+export default class Todo extends Component {
+  render() {
+    <div className="todoListMain">
+      <div className="header">
+        <form>
+          <input placeholder="enter task" />
+          <button type="submit">add</button>
+        </form>
+      </div>
+    </div>;
+  }
+}
